aa8b2bd8de1e91c407fbdc56972096f19af3a76c
Samelijah85 <samelijah85@gmail.com>
2023-09-25 09:38:01 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -13,4 +13,3 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
-
