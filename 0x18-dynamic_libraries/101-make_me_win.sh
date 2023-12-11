566eed524aac4fb79c7f66b05f7fcc04615f0cbc
Samelijah85 <Samelijah85@gmail.com>
2023-12-11 11:58:27 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Samelijah85/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=/tmp/libtest.so
+wget -P /tmp/ https://raw.github.com/Samelijah85/alx-low_level_programming/master/0x18-dynamic_libraries/libdynamic.so
+export LD_PRELOAD=/tmp/libdynamic.so
